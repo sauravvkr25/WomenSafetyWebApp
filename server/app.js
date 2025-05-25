7c3fa7985d3d3e5a2797bcb3a4d791a48d2d337e
akriti0209 <akriti843127@gmail.com>
2025-05-25 14:10:15 +0530

    new changes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 /**
- * Project Name: SocialEcho
+ * Project Name: Connectify
  * Description: A social networking platform with automated content moderation and context-based authentication system.
- *
- * Author: Neaz Mahmud
- * Email: neaz6160@gmail.com
- * Date: 19th June 2023
  */
 
 require("dotenv").config();
